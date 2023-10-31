4dbed556d7d65ee6c281202584e1202c41fb2312
JustinWCola <867581856@qq.com>
2023-10-31 11:24:45 +0800

    feat: add post-process and UI

---
+++ Assets/Scripts/DistanceMeasure.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DistanceMeasure : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Ray ray;
+    public Transform shoot;
+    private TMP_Text text;
+    void Start()
+    {
+        text = GetComponent<TMP_Text>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ray = new Ray(shoot.position, shoot.TransformDirection(0, 0, 1));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            text.text = "Distance: " + hit.distance.ToString();
+        }
+    }
+}
